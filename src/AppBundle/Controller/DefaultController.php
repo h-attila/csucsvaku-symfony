c70c967ef017ed8a1d88a2ebaccb49d7a1ead254
Horvath Attila <horvath.attila.developer@gmail.com>
2018-02-01 21:00:16 +0100

    production deploy 1.0

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -22,6 +22,50 @@ class DefaultController extends Controller
         $orderForm = $this->createForm(OrderFormType::class);
         $messageForm = $this->createForm(MessageFormType::class);
 
+        // order
+        $orderForm->handleRequest($request);
+        if ($orderForm->isSubmitted() && $orderForm->isValid()) {
+            $orderData = $orderForm->getData();
+
+            $message = (new \Swift_Message())
+                ->setSubject('megrendelés visszaigazolás')
+                ->setFrom(['info@csucsvaku.hu' => '[ csucsvaku.hu - a profi fény titka ]'])
+                ->setTo($orderData['email'])
+                ->setBcc('info@csucsvaku.hu')
+                ->setBody(
+                    $this->renderView('emails/orderEmailMessage.html.twig',
+                        array('orderData' => $orderData )),'text/html'
+                );
+
+            $mailer->send($message);
+
+            $this->addFlash('message', 'Köszönjük megrendelést, melyet e-mailben is visszaigazolunk!');
+            return $this->redirect($request->getUri());
+
+        }
+
+        // message
+        $messageForm->handleRequest($request);
+        if ($messageForm->isSubmitted() && $messageForm->isValid()) {
+            $messageData = $messageForm->getData();
+
+            $message = (new \Swift_Message())
+                ->setSubject('üzenet a csucsvaku.hu oldalról')
+                ->setFrom(['info@csucsvaku.hu' => '[ csucsvaku.hu - a profi fény titka ]'])
+                ->setTo($messageData['email'])
+                ->setBcc('info@csucsvaku.hu')
+                ->setBody(
+                    $this->renderView('emails/messageEmailMessage.html.twig',
+                        array('messageData' => $messageData )),'text/html'
+                );
+
+            $mailer->send($message);
+
+            $this->addFlash('message', 'Köszönjük megkeresést, hamarosan válaszolunk');
+            return $this->redirect($request->getUri());
+
+        }
+
         // replace this example code with whatever you need
         return $this->render('base.html.twig', [
             'orderForm' => $orderForm->createView(),
